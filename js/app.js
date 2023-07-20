3ca8ae77cba0eba959b8c5cdc2f21f08947cc30c
inzamulhaque <mdihalif@gmail.com>
2023-07-20 16:30:27 +0600

    Debounce

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const normal_div = document.querySelector(".n_count");
 const throttle_div = document.querySelector(".t_count");
+const debounce_div = document.querySelector(".d_count");
 
 let n_count = 0;
 let t_count = 0;
+let d_count = 0;
 
 let isScrolling = true;
 
@@ -22,7 +24,19 @@ const throttleCount = () => {
   }
 };
 
+let interval;
+
+const debounceCount = () => {
+  clearTimeout(interval);
+
+  interval = setTimeout(() => {
+    d_count++;
+    debounce_div.innerHTML = `Debounce Count ${d_count}`;
+  }, 1000);
+};
+
 const showCount = () => {
   normalCount();
   throttleCount();
+  debounceCount();
 };
